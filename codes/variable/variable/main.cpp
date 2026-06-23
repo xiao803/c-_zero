79f081475147ccc72466888872915031f3bf80e1
xiao803 <2075643202@qq.com>
2026-06-23 21:29:25 +0800

    🎉 初始提交：开始 C++ 学习之旅

---
+++ codes/variable/variable/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main() {
+
+	double pai_val = 3.14;
+	int val_int = 5;
+	bool is_true = -100;
+	bool is_false = false;
+	std::cout << "pai_val:" << pai_val << std::endl;
+	std::cout << "val_int:" << val_int << std::endl;
+	std::cout << "is_true:" << is_true << std::endl;
+	std::cout << "is_false:" << is_false << std::endl;
+
+	return 0;
+}
